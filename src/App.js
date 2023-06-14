e629d77c44bb039f0383dca27e5dfee10c7fe567
semen <mish_semenov@mail.ru>
2023-06-14 15:07:27 +0300

    Change some import in my Project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
 import ProjectOne from "./pages/ProjectOne";
-import Project from "./Projects/Project";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
 import Contacts from "./pages/Contacts";
@@ -26,7 +25,6 @@ function App() {
               </Routes>
           <Footer/>    
       </Router>
-
     </div>
   );
 }
